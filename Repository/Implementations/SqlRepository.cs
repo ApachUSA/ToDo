0212a4ed416a108ec15d600cfb8d4810a96b2a36
ApachUA <4971779@gmail.com>
2024-01-24 13:16:14 +0200

    Developed IBaseRepository and implemented it

---
+++ Repository/Implementations/SqlRepository.cs
@@ -0,0 +1,43 @@
+﻿using ToDo.Models;
+using ToDo.Repository.Interfaces;
+
+namespace ToDo.Repository.Implementations
+{
+	public class SqlRepository<T> : IBaseRepository<T> where T : EntityBase
+	{
+		private readonly ToDo_DB_Context _dbContext;
+
+		public SqlRepository(ToDo_DB_Context dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public async Task CreateAsync(T entity)
+		{
+			await _dbContext.Set<T>().AddAsync(entity);
+			await _dbContext.SaveChangesAsync();
+		}
+
+		public async Task DeleteAsync(T entity)
+		{
+			_dbContext.Set<T>().Remove(entity);
+			await _dbContext.SaveChangesAsync();
+		}
+
+		public IQueryable<T> GetAll()
+		{
+			return _dbContext.Set<T>();
+		}
+
+		public async Task<T?> GetByIdAsync(Guid id)
+		{
+			return await _dbContext.FindAsync<T>(id);
+		}
+
+		public async Task UpdateAsync(T entity)
+		{
+			_dbContext.Set<T>().Update(entity);
+			await _dbContext.SaveChangesAsync();
+		}
+	}
+}
---
+++ Repository/Interfaces/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using ToDo.Models;
+
+namespace ToDo.Repository.Interfaces
+{
+	public interface IBaseRepository<T> where T : EntityBase
+	{
+		Task CreateAsync(T entity);
+
+		Task DeleteAsync(T entity);
+
+		IQueryable<T> GetAll();
+
+		Task<T?> GetByIdAsync(Guid id);
+
+		Task UpdateAsync(T entity);
+	}
+}
