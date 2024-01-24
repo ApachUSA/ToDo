4baf9e35caa77c5579e922898cf907bce3c707c4
ApachUA <4971779@gmail.com>
2024-01-24 13:14:21 +0200

    Developed dbcontext

---
+++ Repository/ToDo_DB_Context.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.Models;
+
+namespace ToDo.Repository
+{
+	public class ToDo_DB_Context : DbContext
+	{
+		public ToDo_DB_Context(DbContextOptions<ToDo_DB_Context> options) : base(options)
+		{
+			Database.EnsureCreated();
+		}
+
+		public DbSet<ToDoTask> ToDoTasks { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<ToDoTask>(builder =>
+			{
+				builder.ToTable(nameof(ToDoTasks)).HasKey(x => x.Task_ID);
+				builder.Property(x => x.Task_ID).ValueGeneratedOnAdd();
+			});
+		}
+	}
+}
